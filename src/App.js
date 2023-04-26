d6e9f93dc453901b48a3f842e5bc8e2206b0ab5f
torobucci <kevintoro254@gmail.com>
2023-04-26 19:17:55 +0300

    add style to elements

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router';
+import LayOut from './components/view/LayOut';
+import Home from './components/routes/Home';
+import Details from './components/routes/Details';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<LayOut />}>
+        <Route index element={<Home />} />
+        <Route path="/team/:teamName" element={<Details />} />
+      </Route>
+
+    </Routes>
   );
 }
 
